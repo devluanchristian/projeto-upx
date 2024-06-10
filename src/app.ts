a058ffb1fe9854c68b091c3ccf32bcc83240e30e
Luan <luanchristian.rochadocarmo@gmail.com>
2024-06-10 13:04:42 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ const app = fastify()
 // Middleware para adicionar cabeçalhos de CORS
 app.addHook('onRequest', (request, reply, done) => {
   // Configurações básicas de CORS
+
   reply.header('Access-Control-Allow-Origin', '*')
   reply.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
   reply.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
